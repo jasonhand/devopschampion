30cde9bb12a8be78fb1fd5fbd65cb765875b084c
jasonhand <jasonhand@users.noreply.github.com>
2020-02-26 17:08:04 +0000

    deploy: af85bf28e2b36fe6a30cc65364d948262cc40026

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,7 +17,7 @@ var documents = [{
     }, {
     "id": 3,
     "url": "https://jasonhand.github.io/2020/02/26/2020-02-25-Update-Stakeholders.html",
-    "title": "Update Stakeholders",
+    "title": "Updating Stakeholders",
     "body": "2020/02/26 - It’s important that internal teams are aware of what’s happening when an incident occurs. If we don’t provide consistent updates, stakeholders will start coming around and asking. They have every right to this information, but we’ve got to find a better way to make them aware of an issue and what is being done about it. We need to find a way to be clear about our acknowledgment to our internal teams. Clear expectations: We should be clear in presenting what we know, what is being done and what kind of expectations should they have in terms of when they’re going to hear back from us?       Previous   Next   "
     }, {
     "id": 4,
